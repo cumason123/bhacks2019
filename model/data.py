1c50efaf7f2b07236268c97f27782a6cd9033af6
Julius Frost <juliustfrost@gmail.com>
2019-11-16 17:42:56 -0500

    add split

--- model/data.py
+++ model/data.py
@@ -1,6 +1,21 @@
+import numpy as np
+import torch
 from torchvision.datasets import ImageFolder
+from torch.utils.data import Subset
 
 
 def load_dataset(data_folder):
     image_dataset = ImageFolder(data_folder)
     return image_dataset
+
+
+def split(dataset, seed=0, train_split=0.8, valid_split=0.1, test_split=0.1):
+    np.random.seed(seed)
+    indices = np.arange(len(dataset))
+    np.random.shuffle(indices)
+    i = int(len(dataset)*train_split)
+    j = int(len(dataset)*(train_split+valid_split))
+    train = Subset(dataset, indices[:i])
+    valid = Subset(dataset, indices[i:j])
+    test = Subset(dataset, indices[j:])
+    return train, valid, test
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,9 +1,15 @@
 import os
 
-from model.data import load_dataset
+from model.data import load_dataset, split
+
+data_folder = 'formatted_data'
 
 
 def test_load_dataset():
-    data_folder = 'formatted_data'
     if os.path.exists(data_folder):
-        load_dataset(data_folder)
+        dataset = load_dataset(data_folder)
+
+
+def test_split():
+    if os.path.exists(data_folder):
+        train, valid, test = split(load_dataset(data_folder))
