012da8407d733590dd4998b02a27bc296ad7343f
Julius Frost <juliustfrost@gmail.com>
2019-11-16 23:56:14 -0500

    merge build model function

--- classifier/model.py
+++ classifier/model.py
@@ -3,13 +3,6 @@ import torch.nn as nn
 from torchvision.models.resnet import resnet18
 
 
-def build_model(num_classes, model_function):
-    model = model_function(pretrained=True)
-    num_features = model.fc.in_features
-    model.fc = nn.Linear(num_features, num_classes)
-    return model
-
-
 class Model(nn.Module):
     def save(self, path):
         torch.save(self.state_dict(), path)
@@ -21,7 +14,10 @@ class Model(nn.Module):
 class TransferModel(Model):
     def __init__(self, pretrained_model_function, num_classes):
         super().__init__()
-        self.model = build_model(num_classes, pretrained_model_function)
+        model = pretrained_model_function(pretrained=True)
+        num_features = model.fc.in_features
+        model.fc = nn.Linear(num_features, num_classes)
+        self.model = model
 
     def forward(self, x):
         return self.model(x)
