b43099d36e00b1022708d94531c6e2fe153fa5e6
Julius Frost <juliustfrost@gmail.com>
2019-11-16 20:53:52 -0500

    change data

--- model/data.py
+++ model/data.py
@@ -9,12 +9,12 @@ def load_dataset(data_folder):
     return image_dataset
 
 
-def split(dataset, seed=0, train_split=0.8, valid_split=0.1, test_split=0.1):
+def split(dataset, seed=0, valid_split=0.1, test_split=0.1):
     np.random.seed(seed)
     indices = np.arange(len(dataset))
     np.random.shuffle(indices)
-    i = int(len(dataset)*train_split)
-    j = int(len(dataset)*(train_split+valid_split))
+    i = int(len(dataset) * (1 - valid_split - test_split))
+    j = int(len(dataset) * (1 - test_split))
     train = Subset(dataset, indices[:i])
     valid = Subset(dataset, indices[i:j])
     test = Subset(dataset, indices[j:])
