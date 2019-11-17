dc2601819de049e94ff0992b823dcb0159ccd657
Curtis Mason <cumason@bu.edu>
2019-11-17 10:24:25 -0500

    Cleaned server

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ from werkzeug.utils import secure_filename
 import numpy as np
 
 import matplotlib.pyplot as plt
+from classifier.model import TransferModel
 
+skin_classifier = TransferModel(model_cls, len(test_dataset.classes))
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
@@ -19,17 +21,16 @@ def upload_file():
             return redirect(request.url)
 
         file = request.files['file']
- 
-        if True in [file.endswith(extension) for extension in ALLOWED_EXTENSIONS]
+
+        if True in [file.filename.endswith(extension) for extension in ALLOWED_EXTENSIONS]:
             filestr = file.read()
             data = np.frombuffer(filestr, np.uint8)
             img = cv2.imdecode(data, cv2.IMREAD_COLOR)
-            reshaped_img = cv2.cvtColor(reshaped_img, cv2.COLOR_BGR2RGB)
+            reshaped_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            resp = evaluate(np.array(reshaped_img))
 
-
-            return Response({'type': 'yes'}, status=200, mimetype="application/json")
-            
-    return  Response({'type': 'yes'}, status=200, mimetype="application/json")
+            return Response({'type': resp}, status=200, mimetype="application/json")
+    return  Response([], status=200, mimetype="application/json")
 
 if (__name__ == '__main__'):
-    app.run('127.0.0.1', port=3000)
+    app.run('0.0.0.0', port=3000)
