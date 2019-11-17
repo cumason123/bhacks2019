995b523a150167eb6fb0bcf7d99f44aa02f05e9f
Julius Frost <juliustfrost@gmail.com>
2019-11-16 20:19:05 -0500

    update for model

---
+++ tests/test_train.py
@@ -0,0 +1,9 @@
+from model.train import *
+from model.model import Resnet
+
+
+def test_build_model():
+    num_classes = 10
+    model = Resnet(num_classes)
+    output = model(torch.zeros(1, 3, 100, 100))
+    assert num_classes == output.size(1)
