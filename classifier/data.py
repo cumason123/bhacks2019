cb3f14aa8cc949cd02c31fb112699f33f10351b9
Julius Frost <juliustfrost@gmail.com>
2019-11-16 22:58:53 -0500

    change to new balanced data format

--- classifier/data.py
+++ classifier/data.py
@@ -1,40 +1,21 @@
-import numpy as np
-import torch
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
-from torch.utils.data import Subset
 
 
+def load_dataset(data_folder, split='train'):
+    if split == 'train':
+        trans = transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+    else:
+        trans = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
 
-
-
-
-def load_dataset(data_folder):
-    train = transforms.Compose([
-        transforms.RandomResizedCrop(224),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
-
-    val = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
-
-    image_dataset = ImageFolder(data_folder)
-    return image_dataset
-
-
-def split(dataset, seed=0, valid_split=0.1, test_split=0.1):
-    np.random.seed(seed)
-    indices = np.arange(len(dataset))
-    np.random.shuffle(indices)
-    i = int(len(dataset) * (1 - valid_split - test_split))
-    j = int(len(dataset) * (1 - test_split))
-    train = Subset(dataset, indices[:i])
-    valid = Subset(dataset, indices[i:j])
-    test = Subset(dataset, indices[j:])
-    return train, valid, test
+    return ImageFolder(data_folder, transform=trans)
