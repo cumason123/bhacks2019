608f37135ca3ff69305788261edea628bb553d23
Julius Frost <juliustfrost@gmail.com>
2019-11-17 00:30:34 -0500

    load data, added test

--- classifier/data.py
+++ classifier/data.py
@@ -1,21 +1,31 @@
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
+from torchvision.transforms.functional import Image
+
+TRAIN_TRANSFORMS = transforms.Compose([
+    transforms.RandomResizedCrop(224),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+TEST_TRANSFORMS = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+
+def load_image(file):
+    image = Image.open(file)
+    return TEST_TRANSFORMS(image)
 
 
 def load_dataset(data_folder, split='train'):
     if split == 'train':
-        trans = transforms.Compose([
-            transforms.RandomResizedCrop(224),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-        ])
+        transform = TRAIN_TRANSFORMS
     else:
-        trans = transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-        ])
+        transform = TEST_TRANSFORMS
 
-    return ImageFolder(data_folder, transform=trans)
+    return ImageFolder(data_folder, transform=transform)
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,6 +1,6 @@
 import os
 
-from classifier.data import load_dataset
+from classifier.data import load_dataset, load_image
 
 DATA_FOLDER = 'balanced_data'
 
@@ -8,3 +8,15 @@ DATA_FOLDER = 'balanced_data'
 def test_load_dataset():
     if os.path.exists(DATA_FOLDER):
         dataset = load_dataset(DATA_FOLDER)
+
+
+def test_load_image():
+    if os.path.exists(DATA_FOLDER):
+        split = os.listdir(DATA_FOLDER)[0]
+        folder = os.path.join(DATA_FOLDER, split)
+        cls = os.listdir(folder)[0]
+        folder = os.path.join(folder, cls)
+        file = os.listdir(folder)[0]
+        path = os.path.join(folder, file)
+        image = load_image(path)
+        assert image.size(0) == 3 and image.size(1) == 224 and image.size(2) == 224
